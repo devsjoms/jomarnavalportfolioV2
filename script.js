4d998e99540d60b112d9ad3be8f273f5bc81fb0d
jomsGutHib <jomarnaval023@gmail.com>
2025-09-03 22:31:24 +0800

    update

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+  var nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";  
+  } else {
+    nav.className = "topnav";        
+  }
+}
+
+// auto close on click
+document.querySelectorAll('.topnav a:not(.icon)').forEach(link => {
+  link.addEventListener('click', () => {
+    var nav = document.getElementById("myTopnav");
+    nav.classList.remove("responsive");
+  });
+});
+
